f9d68de1fe90ae00aa4322d08a47c10b41ca6701
muzard <arttu.kokki@gmail.com>
2023-09-12 16:08:00 +0300

    unicafe osa 1.9

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -10,6 +10,30 @@ const Button = ({ handleClick, text }) => {
 
 const Display = ({ type, number }) => <div>{type}: {number}</div>
 
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + bad + neutral
+  let average = ( good - bad ) / total
+  const positive = (good / total * 100 || '0')
+  const positiveString = (positive.toString() + ' %')
+
+  if ( total === 0 ) {
+    return (
+      <h4>No feedback given</h4>
+    )
+  }
+
+  return (
+    <div>
+      <Display type={'good'} number={good} />
+      <Display type={'neutral'} number={neutral} />
+      <Display type={'bad'} number={bad} />
+      <Display type={'all'} number={total} />
+      <Display type={'average'} number={(average || '0')} />
+      <Display type={'positive'} number={positiveString} />
+    </div>
+  )
+}
+
 const App = () => {
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
@@ -39,13 +63,8 @@ const App = () => {
         <Button handleClick={handleNeutralClick} text={'neutral'} />
         <Button handleClick={handleBadClick} text={'bad'} />
       </div>
-
       <div>
-        <h1>statistics</h1>
-
-        <Display type={'Good'} number={good} />
-        <Display type={'Neutral'} number={neutral} />
-        <Display type={'Bad'} number={bad} />
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   )
