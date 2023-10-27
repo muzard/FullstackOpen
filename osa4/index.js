96023ba8a225e71d19d35bb444619c6de6b08045
muzard <arttu.kokki@gmail.com>
2023-10-27 17:39:03 +0300

    make model to its own file

--- osa4/index.js
+++ osa4/index.js
@@ -3,16 +3,8 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Blog = require("./models/blog");
 const mongoose = require("mongoose");
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl);
