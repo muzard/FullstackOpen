86fb14346096275e68d40a62d9d53414c135b86c
muzard <arttu.kokki@gmail.com>
2023-10-27 21:28:13 +0300

    tests step3

--- osa4/utils/list_tester.js
+++ osa4/utils/list_tester.js
@@ -2,6 +2,29 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const reducer = (likes, item) => {
+    return likes + item.likes;
+  };
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  let returnable;
+  let highestLikes = -1;
+
+  blogs.forEach((blog) => {
+    if (blog.likes > highestLikes) {
+      returnable = blog;
+      highestLikes = blog.likes;
+    }
+  });
+  return returnable;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 };
