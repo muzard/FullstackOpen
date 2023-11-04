dd31fea80039a674edb30a3c4eb715e0d6169d55
muzard <arttu.kokki@gmail.com>
2023-11-04 16:47:03 +0200

    create first api tests

--- osa3/tests/notes_api.test.js
+++ osa3/tests/notes_api.test.js
@@ -4,11 +4,31 @@ const app = require("../app");
 
 const api = supertest(app);
 
-test.only("notes are returned as json", async () => {
-  await api
-    .get("/api/notes")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("api tests", () => {
+  test("notes are returned as json", async () => {
+    await api
+      .get("/api/notes")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("there are two notes", async () => {
+    const response = await api.get("/api/notes");
+
+    expect(response.body).toHaveLength(2);
+  });
+
+  test("the first note is about this part", async () => {
+    const response = await api.get("/api/notes");
+
+    expect(response.body[0].content).toBe("This part of the course is funked");
+  });
+
+  test("the second note is important", async () => {
+    const response = await api.get("/api/notes");
+
+    expect(response.body[1].important).toBe(true);
+  });
 });
 
 afterAll(async () => {
