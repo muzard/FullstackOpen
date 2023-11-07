033a68601584fb297dd6694e25db484a7e3c93dc
muzard <arttu.kokki@gmail.com>
2023-11-07 13:51:33 +0200

    ex4.15

--- osa4/app.js
+++ osa4/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const { info, error } = require("./utils/logger");
 const mongoose = require("mongoose");
 
@@ -25,5 +26,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,6 +2,15 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 const { info, error } = require("../utils/logger");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -9,7 +18,12 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const user = await User.findById(request.body.userId);
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "invalid token" });
+  }
+  const user = await User.findById(decodedToken.id);
+
   const { title, author, url, likes } = request.body;
   if (!title || !url) {
     response.status(400).end();
