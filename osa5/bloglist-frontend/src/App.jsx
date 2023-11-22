9fbcf885765d1f122f49db573c6284de144bbfa0
muzard <arttu.kokki@gmail.com>
2023-11-22 20:42:22 +0200

    add comp Notification, states and styles

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,84 +1,131 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs' 
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
-  const [title, setTitle] = useState("")
-  const [author, setAuthor] = useState("")
-  const [URL, setURL] = useState("")
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [URL, setURL] = useState("");
+
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [notificationStyle, setNotificationStyle] = useState("");
+
+  const contentStyle = {
+    padding: "6px",
+    fontSize: "28px",
+    margin: "12px 0",
+    border: "2px solid green",
+    backgroundColor: "rgb(226, 226, 226)",
+    color: "green",
+  };
+
+  const errorStyle = {
+    padding: "6px",
+    fontSize: "28px",
+    margin: "12px 0",
+    border: "2px solid red",
+    backgroundColor: "rgb(226, 226, 226)",
+    color: "red",
+  };
+
+  const emptyStyle = {
+    height: "0",
+    width: "0",
+  };
+
+  const Notification = ({ message, style }) => {
+    return <em style={style}>{message}</em>;
+  };
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser")
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      blogService.setToken(user.token)
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
     }
-  }, [])
-  
-  const logOut = () => {
-    setUser(null)
-    blogService.setToken(null)
-    window.localStorage.removeItem("loggedBlogappUser")
-  }
+  }, []);
 
-  const createBlog = async event => {
-    event.preventDefault()
-    const blog = await blogService.create(title, author, URL)
-    setBlogs(blogs.concat(blog))
-    setTitle("")
-    setAuthor("")
-    setURL("")
-  }
+  const logOut = () => {
+    setUser(null);
+    blogService.setToken(null);
+    window.localStorage.removeItem("loggedBlogappUser");
+  };
+
+  const createBlog = async (event) => {
+    event.preventDefault();
+    const blog = await blogService.create(title, author, URL);
+    setBlogs(blogs.concat(blog));
+    setTitle("");
+    setAuthor("");
+    setURL("");
+  };
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const user = await blogService.login({username, password})
-    setUser(user)
-    blogService.setToken(user.token)
+    const user = await blogService.login({ username, password });
+    setUser(user);
+    blogService.setToken(user.token);
 
-    window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user))
+    window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
 
-    setUsername("")
-    setPassword("")
-  }
+    setUsername("");
+    setPassword("");
+  };
 
   const userLoggedInContent = () => {
-      return (
+    return (
       <div>
-        <div>
-          Logged in as {user.name}
-        </div>
+        <div>Logged in as {user.name}</div>
 
         <div>
           <h2>blogs</h2>
-          {blogs.map(blog =>
+          {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
-          )}
+          ))}
         </div>
 
         <div>
           <h2>Create new blog</h2>
           <form onSubmit={createBlog}>
-            <div>title <input type="text" name="title" value={title} onChange={({ target }) => setTitle(target.value)} />
+            <div>
+              title{" "}
+              <input
+                type="text"
+                name="title"
+                value={title}
+                onChange={({ target }) => setTitle(target.value)}
+              />
             </div>
 
-            <div>author <input type="text" name="author" value={author} onChange={({ target }) => setAuthor(target.value)} />
+            <div>
+              author{" "}
+              <input
+                type="text"
+                name="author"
+                value={author}
+                onChange={({ target }) => setAuthor(target.value)}
+              />
             </div>
 
-            <div>URL <input type="text" name="URL" value={URL} onChange={({ target }) => setURL(target.value)} />
+            <div>
+              URL{" "}
+              <input
+                type="text"
+                name="URL"
+                value={URL}
+                onChange={({ target }) => setURL(target.value)}
+              />
             </div>
 
             <button type="submit">create</button>
@@ -87,8 +134,8 @@ const App = () => {
 
         <br />
       </div>
-    )
-  }
+    );
+  };
 
   const userNotLoggedInContent = () => {
     return (
@@ -96,26 +143,44 @@ const App = () => {
         <h1>log in to application</h1>
 
         <form onSubmit={handleLogin}>
-          <div>username<input type="text" name="username" value={username} onChange={({ target }) => setUsername(target.value)}/></div>
-          <div>password<input type="text" name="password" value={password} onChange={({ target }) => setPassword(target.value)}/></div>
+          <div>
+            username
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="text"
+              name="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
           <button type="submit">login</button>
         </form>
-
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div>
       <h1>blogsite</h1>
 
+      <div>
+        <Notification message={notificationMessage} style={notificationStyle} />
+      </div>
+
       {!user && userNotLoggedInContent()}
       {user && userLoggedInContent()}
 
-
       <button onClick={logOut}>log out</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
